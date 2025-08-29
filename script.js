0a6f42a0823be91bddf747af14dc848b138b1a97
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-08-29 19:08:11 +0600

    Implemented like counts feature

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('connected')
+// handle like counts on click -> heart icon
+let likes = 0;
+const heartIcons = document.getElementsByClassName('heart-icon');
+for(const icon of heartIcons){
+    icon.addEventListener('click', function(){
+        likes++;
+        updateLikes();
+    })
+}
+
+// update likes (heart) count
+const likesField = document.getElementById('likes-count');
+function updateLikes(){
+    likesField.innerText = likes;
+}
